be7a1767dbacbe0bf262ff4cd06d2b976007c211
KingBob <52259912+Kingbobs@users.noreply.github.com>
2020-06-07 13:17:53 -0600

    Update PrisonCells.php

--- src/PrisonCells/PrisonCells.php
+++ src/PrisonCells/PrisonCells.php
@@ -128,9 +128,8 @@ class PrisonCells extends PluginBase implements Listener{
 	}
 
 	public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
-		if($command->getName() === "command"){
+		switch($cmd->getName())
 			return true;
-		}
 			case "cell":
 				if($sender instanceof Prisoner){
 					if(!empty($args[0])){
